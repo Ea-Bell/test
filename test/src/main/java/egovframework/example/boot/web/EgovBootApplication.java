6714e088221eb54bc1008dac8275baaabc209d32
Ea-Bell <55992288+Ea-Bell@users.noreply.github.com>
2021-12-24 02:08:44 +0900

    eabell
    test

---
+++ test/src/main/java/egovframework/example/boot/web/EgovBootApplication.java
@@ -0,0 +1,22 @@
+package egovframework.example.boot.web;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource({"classpath:/egovframework/springmvc/dispatcher-servlet.xml","classpath*:/egovframework/spring/context-*.xml"})
+@Import(EgovBootInitialization.class)
+public class EgovBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication springApplication = new SpringApplication(EgovBootApplication.class);
+		springApplication.setBannerMode(Banner.Mode.OFF);
+		springApplication.setLogStartupInfo(false);
+		springApplication.run(args);
+	}
+
+}
